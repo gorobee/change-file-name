efa4cf25e49461ab7d35cc79918be719038d666c
gorobee <gorobee.5656@gmail.com>
2021-05-05 00:05:56 +0900

    delete message of purpose from main-code.

--- fileNameChanger.sh
+++ fileNameChanger.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Created date: 2020/04/19
-# Updated date: 2021/05/04
-# This code changes the file-name in the current directory from XXX.csv to i.csv. (i=1,2,3,...)
-
-
 ###################### Declaration
 beforeFiles=()
 afterFiles=()
